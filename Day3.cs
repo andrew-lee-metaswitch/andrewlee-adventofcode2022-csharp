7e3f297543b709a2292198321f1c795dcce73d4e
Andrew Lee <andrewlee@microsoft.com>
2022-12-04 11:31:07 +0000

    Day 3 solve

--- Day3.cs
+++ Day3.cs
@@ -1,19 +1,67 @@
-﻿namespace andrewlee_adventofcode2022_csharp
+﻿using System.Diagnostics;
+using System.Text.Unicode;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
+using System.Threading.Channels;
+
+namespace andrewlee_adventofcode2022_csharp
 {
     public class Day3 : Day
     {
         public override int Part1Solve()
         {
             string[] inputs = LoadInputs("202203");
-            return 0;
+
+            int prioritySum = 0;
+            foreach (string input in inputs)
+            {
+                char[] rucksackChars = input.ToCharArray();
+
+                (List<char>, List<char>) rucksackCompartments = (
+                    rucksackChars.Take(rucksackChars.Length / 2).ToList(),
+                    rucksackChars.Skip(rucksackChars.Length / 2).ToList());
+
+                char commonChar = rucksackCompartments.Item1.Where(c => rucksackCompartments.Item2.Contains(c)).FirstOrDefault();
+                prioritySum += charValue(commonChar);
+            }
+            return prioritySum;
 
         }
 
+        private int charValue (char commonChar) {
+            int returnValue = (int) commonChar;
+            if (65 <= (int) commonChar && (int) commonChar <= 90)
+            {
+                // 65 = A -> 27
+                returnValue -= 38;
+            } else
+            {
+                // 97 = a -> 1
+                returnValue -= 96;
+            }
+            return returnValue;
+        }
 
         public override int Part2Solve()
         {
             string[] inputs = this.LoadInputs("202203");
-            return 0;
+            int prioritySum = 0;
+            int index = 0;
+            List<List<char>> rucksackCompartments = new List<List<char>> { };
+            foreach (string input in inputs)
+            {
+                rucksackCompartments.Add(input.ToCharArray().ToList());
+                if (index % 3 == 2)
+                {
+                    char commonChar = rucksackCompartments[0].Where(
+                        c => rucksackCompartments[1].Contains(c)).Where(
+                        c => rucksackCompartments[2].Contains(c)).FirstOrDefault();
+                    prioritySum += charValue(commonChar);
+                    rucksackCompartments = new List<List<char>> { };
+                }
+                index++;
+            }
+            return prioritySum;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using andrewlee_adventofcode2022_csharp;
 
-//var day1 = new Day1();
-//Console.WriteLine(day1.Part1Solve());
-//Console.WriteLine(day1.Part2Solve());
-var day2 = new Day2();
-Console.WriteLine(day2.Part1Solve());
-Console.WriteLine(day2.Part2Solve());
+//var day = new Day1();
+//var day = new Day2();
+var day = new Day3();
+Console.WriteLine(day.Part1Solve());
+Console.WriteLine(day.Part2Solve());
