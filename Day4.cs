9984106f63c54a8b64354ff3e746959dbd31445a
Andrew Lee <andrewlee@microsoft.com>
2022-12-04 11:50:59 +0000

    Day 4

---
+++ Day4.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace andrewlee_adventofcode2022_csharp
+{
+    public class Day4 : Day
+    {
+        public override int Part1Solve()
+        {
+            string[] inputs = LoadInputs("202204");
+            string pattern = @"^(\d+)-(\d+),(\d+)-(\d+)$";
+
+            int countOfOverlaps = 0;
+            foreach (string input in inputs)
+            {
+                var groups = Regex.Match(input, pattern).Groups.Values.ToList();
+                if (((Int32.Parse(groups[1].Value) <= Int32.Parse(groups[3].Value)) && (Int32.Parse(groups[2].Value) >= Int32.Parse(groups[4].Value))) ||
+                    ((Int32.Parse(groups[3].Value) <= Int32.Parse(groups[1].Value)) && (Int32.Parse(groups[4].Value) >= Int32.Parse(groups[2].Value))))
+                {
+                    countOfOverlaps++;
+                }
+            }
+            return countOfOverlaps;
+        }
+
+        public override int Part2Solve()
+        {
+            string[] inputs = LoadInputs("202204");
+            string pattern = @"^(\d+)-(\d+),(\d+)-(\d+)$";
+
+            int countOfAnyOverlaps = 0;
+            foreach (string input in inputs)
+            {
+                var groups = Regex.Match(input, pattern).Groups.Values.ToList();
+                if (((Int32.Parse(groups[1].Value) <= Int32.Parse(groups[3].Value)) && (Int32.Parse(groups[2].Value) >= Int32.Parse(groups[3].Value))) ||
+                    ((Int32.Parse(groups[3].Value) <= Int32.Parse(groups[1].Value)) && (Int32.Parse(groups[4].Value) >= Int32.Parse(groups[1].Value))))
+                {
+                    countOfAnyOverlaps++;
+                }
+            }
+            return countOfAnyOverlaps;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using andrewlee_adventofcode2022_csharp;
 
 //var day = new Day1();
 //var day = new Day2();
-var day = new Day3();
+//var day = new Day3();
+var day = new Day4();
 Console.WriteLine(day.Part1Solve());
 Console.WriteLine(day.Part2Solve());
